6aec96e64a02addc81e90f94201e25699a89ca32
Gusepe <120711057+Gusepe@users.noreply.github.com>
2024-01-12 10:32:30 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'G&N_s Studio'
+description 'Paleto Bay Mapdata - Sheriff Only'
+version '4.0.0'
+
+dependencies {
+    '/gameBuild:2189'
+}
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/ybn/*.ybn',
+    'stream/ydr/*.ydr',
+    'stream/ydr/*.ydd',
+    'stream/ymap/*.ymap'
+}
+
