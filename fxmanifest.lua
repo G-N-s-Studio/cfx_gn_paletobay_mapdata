46ba8b59f8f3288015bf97624d8b531f3ea9197a
Gusepe <120711057+Gusepe@users.noreply.github.com>
2024-01-13 11:00:03 +0100

    Current - Sheriff + Clinic + BlackWood Saloon

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,19 @@
-fx_version 'cerulean'
-game 'gta5'
+fx_version "cerulean"
+games { 'gta5' }
 
 author 'G&N_s Studio'
-description 'Paleto Bay Mapdata - Sheriff Only'
+description 'Paleto Bay Mapdata - Sheriff + Clinic + BlackWood Saloon'
 version '4.0.0'
 
 dependencies {
     '/gameBuild:2189'
 }
 
+dependency '/gameBuild:2189'
+
 this_is_a_map 'yes'
 
 escrow_ignore {
     'stream/**/*'
 }
+
